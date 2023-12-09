58eba1dc8f29a39a2a7e9bea402596341b29b144
Mostakimul Karim <mostakimulkarim@gmail.com>
2023-12-09 03:22:20 -0500

    [feat]: using pre hook for validation

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -19,6 +19,27 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>(
   },
 )
 
+academicDepartmentSchema.pre('save', async function (next) {
+  const isDepartmentExist = await AcademicDepartment.findOne({
+    name: this.name,
+  })
+  if (isDepartmentExist) {
+    throw new Error('This deprtment is already exist!')
+  }
+  next()
+})
+
+// query
+academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
+  const query = this.getQuery()
+
+  const isDepartmentExist = await AcademicDepartment.findOne(query)
+  if (!isDepartmentExist) {
+    throw new Error("This deprtment is doesn't exist!")
+  }
+  next()
+})
+
 export const AcademicDepartment = model<TAcademicDepartment>(
   'AcademicDepartment',
   academicDepartmentSchema,
--- src/app/modules/academicDepartment/academicDepartment.service.ts
+++ src/app/modules/academicDepartment/academicDepartment.service.ts
@@ -4,6 +4,13 @@ import { AcademicDepartment } from './academicDepartment.model'
 const createacAdemicDepartmentService = async (
   payload: TAcademicDepartment,
 ) => {
+  // const isDepartmentExist = await AcademicDepartment.findOne({
+  //   name: payload.name,
+  // })
+
+  // if (isDepartmentExist) {
+  //   throw new Error('This deprtment is already exist!')
+  // }
   const result = await AcademicDepartment.create(payload)
   return result
 }

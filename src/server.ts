a4161dd472a385bb4b6739e8681882d5f38569c0
Mostakimul Karim <mostakimulkarim@gmail.com>
2023-12-12 03:28:05 -0500

    [feat]: unhandle & uncaught exeptions handling

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,33 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./app/config";
+import { Server } from 'http'
+import mongoose from 'mongoose'
+import app from './app'
+import config from './app/config'
+
+let server: Server
 
 async function main() {
   try {
-    await mongoose.connect(config.databse_url as string);
+    await mongoose.connect(config.databse_url as string)
 
-    app.listen(config.port, () => {
-      console.log(`Server is litsening to port ${config.port}`);
-    });
+    server = app.listen(config.port, () => {
+      console.log(`Server is litsening to port ${config.port}`)
+    })
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 }
 
-main();
+main()
+
+process.on('unhandledRejection', () => {
+  if (server) {
+    server.close(() => {
+      process.exit(1)
+    })
+  }
+  process.exit(1)
+})
+
+process.on('uncaughtException', () => {
+  process.exit(1)
+})

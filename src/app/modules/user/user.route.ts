4d976a493bbfeb27dbb31f3e33b1193e002a5765
Mostakimul Karim <mostakimulkarim@gmail.com>
2023-12-13 08:25:20 -0500

    [feat]: admin and faculty module create

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 import validateRequest from '../../middlewares/validateRequest'
+import { createAdminValidationSchema } from '../admin/admin.validation'
+import { createFacultyValidationSchema } from '../faculty/faculty.validation'
 import { StudentValidations } from '../student/student.validation'
 import { UserController } from './user.controller'
 
@@ -9,6 +11,18 @@ router.post(
   '/create-student',
   validateRequest(StudentValidations.StudentValidationSchema),
   UserController.createStudent,
+)
+
+router.post(
+  '/create-faculty',
+  validateRequest(createFacultyValidationSchema),
+  UserController.createFaculty,
+)
+
+router.post(
+  '/create-admin',
+  validateRequest(createAdminValidationSchema),
+  UserController.createAdmin,
 )
 
 export const UserRoutes = router
